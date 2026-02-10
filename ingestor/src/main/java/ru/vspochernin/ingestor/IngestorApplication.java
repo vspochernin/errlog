529f6190a0e53da5247e3b258bba259827634ff4
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-10 09:50:23 +0300

    initialize ingestor

---
+++ ingestor/src/main/java/ru/vspochernin/ingestor/IngestorApplication.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestorApplication.class, args);
+	}
+
+}
---
+++ ingestor/src/test/java/ru/vspochernin/ingestor/IngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
