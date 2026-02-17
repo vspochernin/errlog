e62697affb1313a118aad801d1679733c68834a9
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-17 20:15:11 +0300

    initialize errapi

---
+++ errapi/src/main/java/ru/vspochernin/errapi/ErrapiApplication.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.errapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrapiApplication.class, args);
+	}
+
+}
