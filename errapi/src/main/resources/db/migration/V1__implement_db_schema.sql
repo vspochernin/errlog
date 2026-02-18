df390ccc55d16cb2123fa126515c0ea316955f4b
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-18 09:09:06 +0300

    update db init script

--- errapi/src/main/resources/db/migration/V1__implement_db_schema.sql
+++ errapi/src/main/resources/db/migration/V1__implement_db_schema.sql
@@ -1,7 +1,17 @@
 CREATE TABLE users
 (
-    id       BIGSERIAL PRIMARY KEY,
-    login    VARCHAR(32)  NOT NULL,
-    email    VARCHAR(255) NOT NULL,
-    password VARCHAR(64)  NOT NULL
+    id            BIGSERIAL PRIMARY KEY,
+
+    login         VARCHAR(64)  NOT NULL,
+    email         VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+
+    role          VARCHAR(16)  NOT NULL,
+    can_search    BOOLEAN      NOT NULL DEFAULT FALSE, -- Админ должен будет разрешить пользователю выполнять запросы.
+    is_active     BOOLEAN      NOT NULL DEFAULT TRUE,  -- Для возможности блокировки пользователя.
+    created_at    TIMESTAMPTZ  NOT NULL DEFAULT now(),
+
+    CONSTRAINT users_login_uk UNIQUE (login),
+    CONSTRAINT users_email_uk UNIQUE (email),
+    CONSTRAINT users_role_chk CHECK (role IN ('USER', 'ADMIN'))
 );
