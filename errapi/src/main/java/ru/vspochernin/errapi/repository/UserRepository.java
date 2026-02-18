3ef2c28436cb9c2e84a58a07abc4337e9dede3aa
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-18 11:24:03 +0300

    add user repository

---
+++ errapi/src/main/java/ru/vspochernin/errapi/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package ru.vspochernin.errapi.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.vspochernin.errapi.model.User;
+import ru.vspochernin.errapi.model.UserRole;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLogin(String login);
+
+    boolean existsByLogin(String login);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByRole(UserRole role);
+}
