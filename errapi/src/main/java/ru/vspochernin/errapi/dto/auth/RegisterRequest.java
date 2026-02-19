6b5d626bef18c5d6bb68801ede9f056641f38ce7
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-19 21:27:06 +0300

    add email validation

--- errapi/src/main/java/ru/vspochernin/errapi/dto/auth/RegisterRequest.java
+++ errapi/src/main/java/ru/vspochernin/errapi/dto/auth/RegisterRequest.java
@@ -1,11 +1,12 @@
 package ru.vspochernin.errapi.dto.auth;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotBlank @Size(min = 3, max = 64) String login,
-        @NotBlank @Size(max = 255) String email,
+        @NotBlank @Email @Size(max = 255) String email,
         @NotBlank @Size(min = 6, max = 128) String password)
 {
 }
