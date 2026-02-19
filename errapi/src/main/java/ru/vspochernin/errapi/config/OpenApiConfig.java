d45400711f7bfdc78ade5ead7058e922e8d58882
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-19 11:51:57 +0300

    add custom open api bean

--- errapi/src/main/java/ru/vspochernin/errapi/config/OpenApiConfig.java
+++ errapi/src/main/java/ru/vspochernin/errapi/config/OpenApiConfig.java
@@ -2,8 +2,12 @@ package ru.vspochernin.errapi.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Для появления в Swagger UI кнопки "Authorize".
 @Configuration
 @SecurityScheme(
         name = "bearerAuth",
@@ -11,4 +15,11 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer",
         bearerFormat = "JWT")
 public class OpenApiConfig {
+
+    // Чтобы Swagger начал прикревлять токен к запросам.
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
 }
