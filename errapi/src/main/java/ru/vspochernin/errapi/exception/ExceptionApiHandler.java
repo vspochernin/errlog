66dfd4c8bf8f0a1d1071a5ac71ca26786e256ed5
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-19 22:47:41 +0300

    implement ExceptionApiHandler

---
+++ errapi/src/main/java/ru/vspochernin/errapi/exception/ExceptionApiHandler.java
@@ -0,0 +1,61 @@
+package ru.vspochernin.errapi.exception;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionApiHandler {
+
+    @ExceptionHandler(ErrapiException.class)
+    public ResponseEntity<ErrorMessage> tesseractException(ErrapiException exception) {
+        return ResponseEntity
+                .status(exception.getErrorType().getHttpStatus())
+                .body(ErrorMessage.fromErrapiException(exception));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorMessage> badCredentialsException(BadCredentialsException exception) {
+        ErrapiErrorType errorType = ErrapiErrorType.BAD_CREDENTIALS;
+        return ResponseEntity
+                .status(errorType.getHttpStatus())
+                .body(ErrorMessage.fromErrorTypeWithAdditionalInfo(errorType, exception.toString()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        ErrapiErrorType errorType = ErrapiErrorType.BAD_REQUEST_BODY;
+        return ResponseEntity
+                .status(errorType.getHttpStatus())
+                .body(ErrorMessage.fromErrorTypeWithAdditionalInfo(errorType, exception.toString()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorMessage> httpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        ErrapiErrorType errorType = ErrapiErrorType.BAD_REQUEST_BODY;
+        return ResponseEntity
+                .status(errorType.getHttpStatus())
+                .body(ErrorMessage.fromErrorTypeWithAdditionalInfo(errorType, exception.toString()));
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<ErrorMessage> numberFormatException(NumberFormatException exception) {
+        ErrapiErrorType errorType = ErrapiErrorType.BAD_REQUEST_BODY;
+        return ResponseEntity
+                .status(errorType.getHttpStatus())
+                .body(ErrorMessage.fromErrorTypeWithAdditionalInfo(errorType, exception.toString()));
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorMessage> noSuchElementException(NoSuchElementException exception) {
+        ErrapiErrorType errorType = ErrapiErrorType.NOT_FOUND;
+        return ResponseEntity
+                .status(errorType.getHttpStatus())
+                .body(ErrorMessage.fromErrorTypeWithAdditionalInfo(errorType, exception.toString()));
+    }
+}
