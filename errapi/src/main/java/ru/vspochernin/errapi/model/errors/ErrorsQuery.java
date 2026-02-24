1e1f60133cf686a356f27013f7a2ab52b544e094
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-24 21:57:06 +0300

    fixup comments

--- errapi/src/main/java/ru/vspochernin/errapi/model/errors/ErrorsQuery.java
+++ errapi/src/main/java/ru/vspochernin/errapi/model/errors/ErrorsQuery.java
@@ -3,8 +3,7 @@ package ru.vspochernin.errapi.model.errors;
 import java.time.Instant;
 
 public record ErrorsQuery(
-        // TODO: может добавить fingerprint и список f.
         Instant from,
-        Instant to)
+        Instant to) // TODO: Добавить fingerprint и список f.
 {
 }
