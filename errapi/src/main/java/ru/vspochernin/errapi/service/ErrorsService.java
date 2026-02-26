f4245fa7645f61837576c8b0dbc853fa19c78597
Vladislav Pochernin <vspochernin@gmail.com>
2026-02-26 12:53:00 +0300

    extract limit offset validation to another class

--- errapi/src/main/java/ru/vspochernin/errapi/service/ErrorsService.java
+++ errapi/src/main/java/ru/vspochernin/errapi/service/ErrorsService.java
@@ -8,17 +8,13 @@ import ru.vspochernin.errapi.config.ErrorsAllowlist;
 import ru.vspochernin.errapi.dto.errors.ErrorsRequest;
 import ru.vspochernin.errapi.dto.errors.ErrorsEventsResponse;
 import ru.vspochernin.errapi.dto.errors.ErrorsFiltersResponse;
-import ru.vspochernin.errapi.exception.ErrapiErrorType;
-import ru.vspochernin.errapi.exception.ErrapiException;
 import ru.vspochernin.errapi.model.errors.ErrorsQuery;
 import ru.vspochernin.errapi.repository.ErrorsRepository;
+import ru.vspochernin.errapi.util.ValidationUtils;
 
 @Service
 @RequiredArgsConstructor
 public class ErrorsService {
-
-    private static final int LIMIT_MIN = 1;
-    private static final int LIMIT_MAX = 500;
 
     private final ErrorsRepository errorsRepository;
 
@@ -29,7 +25,7 @@ public class ErrorsService {
     }
 
     public ErrorsEventsResponse getEvents(ErrorsRequest request, int limit, long offset) {
-        validateLimitOffset(limit, offset);
+        ValidationUtils.validateLimitOffset(limit, offset);
 
         ErrorsQuery query = ErrorsQuery.parseFromErrorsRequest(request);
 
@@ -39,15 +35,5 @@ public class ErrorsService {
                 .toList();
 
         return new ErrorsEventsResponse(items, eventsTotal);
-    }
-
-    private static void validateLimitOffset(int limit, long offset) {
-        if (limit < LIMIT_MIN || limit > LIMIT_MAX) {
-            throw new ErrapiException(ErrapiErrorType.BAD_REQUEST,
-                    "Limit должен быть от " + LIMIT_MIN + " до " + LIMIT_MAX);
-        }
-        if (offset < 0L) {
-            throw new ErrapiException(ErrapiErrorType.BAD_REQUEST, "Offset должен быть неотрицательным");
-        }
     }
 }
---
+++ errapi/src/main/java/ru/vspochernin/errapi/util/ValidationUtils.java
@@ -0,0 +1,24 @@
+package ru.vspochernin.errapi.util;
+
+import ru.vspochernin.errapi.exception.ErrapiErrorType;
+import ru.vspochernin.errapi.exception.ErrapiException;
+
+public class ValidationUtils {
+
+    private static final int LIMIT_MIN = 1;
+    private static final int LIMIT_MAX = 500;
+
+    private ValidationUtils() {
+    }
+
+    public static void validateLimitOffset(int limit, long offset) {
+        if (limit < LIMIT_MIN || limit > LIMIT_MAX) {
+            throw new ErrapiException(
+                    ErrapiErrorType.BAD_REQUEST,
+                    "Limit must be from " + LIMIT_MIN + " to " + LIMIT_MAX);
+        }
+        if (offset < 0L) {
+            throw new ErrapiException(ErrapiErrorType.BAD_REQUEST, "Offset can't be negative");
+        }
+    }
+}
