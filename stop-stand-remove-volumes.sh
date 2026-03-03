9f5b07848d04ae09e5c727b932315c739bda0ecf
Vladislav Pochernin <vspochernin@gmail.com>
2026-03-03 13:44:14 +0300

    remove legacy docker compose

--- stop-stand-remove-volumes.sh
+++ stop-stand-remove-volumes.sh
@@ -5,6 +5,3 @@ docker compose -f docker-compose.demo.yml down -v --remove-orphans
 
 echo "Stop core"
 docker compose -f docker-compose.core.yml down -v --remove-orphans
-
-echo "Stop connected stand"
-docker compose -f docker-compose.yml down -v --remove-orphans
--- stop-stand.sh
+++ stop-stand.sh
@@ -6,5 +6,3 @@ docker compose -f docker-compose.demo.yml down --remove-orphans
 echo "Stop core"
 docker compose -f docker-compose.core.yml down --remove-orphans
 
-echo "Stop connected stand"
-docker compose -f docker-compose.yml down --remove-orphans
