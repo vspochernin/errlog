2dc9a65ae6c85a78ea44031ee5eadb0758b8f3e4
Vladislav Pochernin <vspochernin@gmail.com>
2026-03-29 14:18:06 +0300

    add swagger annotations for RegisterRequest

--- errapi/src/main/java/ru/vspochernin/errapi/dto/auth/RegisterRequest.java
+++ errapi/src/main/java/ru/vspochernin/errapi/dto/auth/RegisterRequest.java
@@ -1,12 +1,27 @@
 package ru.vspochernin.errapi.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "Запрос на регистрацию")
 public record RegisterRequest(
-        @NotBlank @Size(min = 3, max = 64) String login,
-        @NotBlank @Email @Size(max = 255) String email,
-        @NotBlank @Size(min = 6, max = 128) String password)
+
+        @Schema(description = "Логин", example = "new_user")
+        @NotBlank
+        @Size(min = 3, max = 64)
+        String login,
+
+        @Schema(description = "Email", example = "newuser@example.com")
+        @NotBlank
+        @Email
+        @Size(max = 255)
+        String email,
+
+        @Schema(description = "Пароль", example = "new_user_password")
+        @NotBlank
+        @Size(min = 6, max = 128)
+        String password)
 {
 }
