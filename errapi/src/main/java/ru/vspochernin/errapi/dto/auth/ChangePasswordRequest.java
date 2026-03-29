3daf1f19aa0eca1c2fca41d5994e3cd38f636d88
Vladislav Pochernin <vspochernin@gmail.com>
2026-03-29 13:36:57 +0300

    add swagger annotations for ChangePasswordRequest

--- errapi/src/main/java/ru/vspochernin/errapi/dto/auth/ChangePasswordRequest.java
+++ errapi/src/main/java/ru/vspochernin/errapi/dto/auth/ChangePasswordRequest.java
@@ -1,10 +1,19 @@
 package ru.vspochernin.errapi.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "Запрос на смену пароля пользователя")
 public record ChangePasswordRequest(
-        @NotBlank String oldPassword,
-        @NotBlank @Size(min = 6, max = 128) String newPassword)
+
+        @Schema(description = "Текущий пароль", example = "old_password")
+        @NotBlank
+        String oldPassword,
+
+        @Schema(description = "Новый пароль", example = "new_password")
+        @NotBlank
+        @Size(min = 6, max = 128)
+        String newPassword)
 {
 }
