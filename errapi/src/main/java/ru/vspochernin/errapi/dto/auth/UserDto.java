463201f2d6f75c266dee39fad8726c2205153f30
Vladislav Pochernin <vspochernin@gmail.com>
2026-03-29 14:19:30 +0300

    add swagger annotations for UserDto

--- errapi/src/main/java/ru/vspochernin/errapi/dto/auth/UserDto.java
+++ errapi/src/main/java/ru/vspochernin/errapi/dto/auth/UserDto.java
@@ -1,12 +1,22 @@
 package ru.vspochernin.errapi.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import ru.vspochernin.errapi.model.auth.User;
 import ru.vspochernin.errapi.model.auth.UserRole;
 
+@Schema(description = "Пользователь сервиса")
 public record UserDto(
+
+        @Schema(description = "Идентификатор пользователя", example = "1")
         long id,
+
+        @Schema(description = "Логин пользователя", example = "owner")
         String login,
+
+        @Schema(description = "Email пользователя", example = "owner@example.com")
         String email,
+
+        @Schema(description = "Роль пользователя", example = "OWNER")
         UserRole role)
 {
 
