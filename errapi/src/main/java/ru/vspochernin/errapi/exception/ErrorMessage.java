1af1feefd1297775de706225a521c05d4387f1e7
Vladislav Pochernin <vspochernin@gmail.com>
2026-03-29 13:33:47 +0300

    add swagger annotations for ErrorMessage

--- errapi/src/main/java/ru/vspochernin/errapi/exception/ErrorMessage.java
+++ errapi/src/main/java/ru/vspochernin/errapi/exception/ErrorMessage.java
@@ -1,9 +1,20 @@
 package ru.vspochernin.errapi.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Единый формат ошибки API")
 public record ErrorMessage(
+
+        @Schema(description = "Числовой идентификатор типа ошибки", example = "7")
         int id,
+
+        @Schema(description = "Описание ошибки", example = "Incorrect role change")
         String description,
+
+        @Schema(description = "Тип ошибки", example = "INCORRECT_ROLE_CHANGE")
         String errorType,
+
+        @Schema(description = "Дополнительная информация об ошибке", example = "Can't change owner role")
         String additionalInfo)
 {
 
