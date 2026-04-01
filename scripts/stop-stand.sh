b9820ff979a158ec618906bfeecd3cf789958c99
Vladislav Pochernin <vspochernin@gmail.com>
2026-04-01 13:40:29 +0300

    move .env and fix scripts

--- scripts/stop-stand.sh
+++ scripts/stop-stand.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Stop demo"
-docker compose -f docker-compose.demo.yml down --remove-orphans
+docker compose -f docekr/docker-compose.demo.yml down --remove-orphans
 
 echo "Stop core"
-docker compose -f docker-compose.core.yml down --remove-orphans
+docker compose -f docker/docker-compose.core.yml down --remove-orphans
 
